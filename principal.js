124081be5f46e39d5e1f23611b8b1c02c07a5699
Miguel <luis.34077@aluno.iffar.edu.br>
2026-08-19 20:28:22 -0300

    Commit terceiro

---
+++ principal.js
@@ -0,0 +1,9 @@
+var banners = ["Os melhores do brasil", "Qualidade e preço bom"]; 
+var indiceBanner = 0; 
+
+function trocarbanner() { 
+    indiceBanner = (indiceBanner + 1) % banners.length; 
+    document.querySelector("h2.mensagem").textContent = banners[indiceBanner]; 
+} 
+
+setInterval(trocarbanner, 2000);
