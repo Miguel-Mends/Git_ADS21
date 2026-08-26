be846acfae05a37c73acd264a11dd519edcdf709
Miguel <luis.34077@aluno.iffar.edu.br>
2026-08-26 20:00:55 -0300

    Revert "commit paragrafo"
    This reverts commit b99553fe6a84f107b787ca87cd08a9f9b6e3b42b.

--- principal.js
+++
@@ -1,9 +0,0 @@
-var banners = ["Os melhores do brasil", "Qualidade e preço bom"]; 
-var indiceBanner = 0; 
-
-function trocarbanner() { 
-    indiceBanner = (indiceBanner + 1) % banners.length; 
-    document.querySelector("h2.mensagem").textContent = banners[indiceBanner]; 
-} 
-
-setInterval(trocarbanner, 1000);
